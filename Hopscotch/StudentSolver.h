51df6d47af5f8421ac3327b4d9359b1a11c21b8c
Yifan Yu <kenryu422@outlook.com>
2022-09-17 03:23:18 -0700

    Remove: All files

--- Hopscotch/StudentSolver.h
+++
@@ -1,8 +0,0 @@
-#ifndef STUDENTSOVLER_H
-#define STUDENTSOLVER_H
-#include <vector>
-class StudentSolver {
- public:
-  static std::vector<int> solve(std::vector<std::vector<int>> grid);
-};
-#endif
